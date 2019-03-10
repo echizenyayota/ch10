3eb6da8f347dd51d6fd2be1c32c9569b8253d8e5
echizenyayota <virtualmisso30@gmail.com>
2019-03-10 14:33:11 +0900

    レシピ270 通信エラー時の処理を実装したい

---
+++ scripts/fetch_catch.js
@@ -0,0 +1,15 @@
+document.getElementById('btn').addEventListener('click', function (e) {
+    fetch('nothing.php')
+      .then(function (response) {
+          if (response.ok) {
+              return response.text();
+          }
+          throw new Error('指定したファイルが存在しません。');
+      })
+      .then(function (data) {
+        document.getElementById('result').textContent = data;
+      });
+      .catch(function(error) {
+          window.alert('Error: ' + error.messgae)
+      });
+}, false);
