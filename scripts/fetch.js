f75f2b22d2ab415a537013c94ef1ee61558eae01
echizenyayota <virtualmisso30@gmail.com>
2019-03-10 14:00:38 +0900

    レシピ269 非同期通信（fetch）でデータを取得したい(その2)

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,10 +1,11 @@
 document.getElementById('btn').addEventListener('click', function (e) {
-fetch('fetch.php')
-// fetch(new Request('fetch.php'))
-  .then(function (response) {
-    return response.text();
-  })
-  .then(function (data) {
-    document.getElementById('result').textContent = data;
-  });
+    console.log(fetch('fetch.php'));
+    fetch('fetch.php')
+    // fetch(new Request('fetch.php'))
+      .then(function (response) {
+        return response.text();
+      })
+      .then(function (data) {
+        document.getElementById('result').textContent = data;
+      });
 }, false);
